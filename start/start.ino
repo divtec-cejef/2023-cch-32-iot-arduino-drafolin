78b00b0069f8150dbfc222000b3e06d277c6110f
drafolin <derg@drafolin.ch>
2024-03-11 15:54:56 +0100

    added temperature measuring

--- start/start.ino
+++ start/start.ino
@@ -1,9 +1,13 @@
+#include <DHT.h>
+#include <DHT_U.h>
 #include <SigFox.h>
+
+DHT dht(5, DHT11);
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
-  };
+  }
   Serial.println("Ready.");
 
   if (!SigFox.begin()) {
@@ -12,13 +16,31 @@ void setup() {
   }
   SigFox.debug();
 
+  pinMode(1, INPUT);
+
+  dht.begin();
+
   /*
   TO DO...
   */
 }
 
 void loop() {
-  /*
-  TO DO...
-  */
+  delay(2000);
+  float temp, hum;
+
+  // Read temperature and humidity
+  hum = dht.readHumidity();
+  temp = dht.readTemperature(false);
+
+  if (isnan(hum) || isnan(temp)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+  }
+
+  Serial.print("btn: ");
+  Serial.println(digitalRead(1));
+  Serial.print("temp: ");
+  Serial.println(temp);
+  Serial.print("hum: ");
+  Serial.println(hum);
 }
