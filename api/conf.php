e02f19dfd44b9ca55189764d16fbbf87e547c283
drafolin <derg@drafolin.ch>
2024-03-13 13:35:14 +0100

    updated api according to db

--- api/conf.php
+++ api/conf.php
@@ -1,8 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] != "POST") {
-	die(405);
-}
-
+global $conn;
 $config = parse_ini_file("db_config.ini");
 $host = $config['HOST'];
 $db = $config['DB'];
@@ -17,11 +14,3 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully\n";
-
-$sql = "INSERT INTO measures VALUES (null, ?, ?, ?);";
-$qry = $conn->prepare($sql);
-if (!$qry->execute([$_POST["t"], $_POST["h"],  gmdate("Y-m-d H:i:s", $_POST["time"])])) {
-	echo $qry->error;
-	die(500);
-}
-echo "Saved successfully";
---
+++ api/lib.php
@@ -0,0 +1,43 @@
+<?php
+require "./conf.php";
+
+function getDeviceId(string $device_id): int|false
+{
+	global $conn;
+	$sql = "SELECT count(*) FROM devices WHERE device_id = ?;";
+	$qry = $conn->prepare($sql);
+	$qry->execute([$device_id]);
+	$res = $qry->get_result();
+	$res = $res->fetch_assoc()["count(*)"];
+	if ($res == 0) {
+		return false;
+	}
+
+	$sql = "SELECT id FROM devices WHERE device_id = ?;";
+	$qry = $conn->prepare($sql);
+	$qry->execute([$device_id]);
+	$res = $qry->get_result();
+	$res = $res->fetch_assoc()["id"];
+	return $res;
+}
+
+function createDevice(string $device_id): int
+{
+	global $conn;
+	$sql = "INSERT INTO devices VALUES (null, ?);";
+	$qry = $conn->prepare($sql);
+	$qry->execute([$device_id]);
+
+	return getDeviceId($device_id);
+}
+
+function saveMeasure(float $temp, float $hum, string $time, int $seq, int $device_id)
+{
+	global $conn;
+	$sql = "INSERT INTO measures VALUES (null, ?, ?, ?, ?, ?);";
+	$qry = $conn->prepare($sql);
+	if (!$qry->execute([$temp, $hum, $time, $seq, $device_id])) {
+		echo $qry->error;
+		die(500);
+	}
+}
