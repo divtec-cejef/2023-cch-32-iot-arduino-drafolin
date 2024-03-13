75357c8c948fb23a3e4c17a05136768ad3798e91
drafolin <derg@drafolin.ch>
2024-03-13 11:51:39 +0100

    modified database and schemes

--- api/migration.sql
+++ api/migration.sql
@@ -1,8 +1,17 @@
-drop table if exists measures;
+DROP TABLE IF EXISTS `devices`; 
+CREATE TABLE `devices` (
+	`id` bigint(20) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`device_id` VARCHAR(6) UNIQUE
+);
+
+drop table if exists `measures`;
 CREATE TABLE `measures` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `temperature` double(8,2) NOT NULL,
   `humidity` double(8,2) NOT NULL,
   `measure_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sequence_number` INT NOT NULL,
+  `device` BIGINT(20) unsigned,
+  FOREIGN KEY (`device`) REFERENCES `devices`(`id`),
   PRIMARY KEY (`id`)
 );
