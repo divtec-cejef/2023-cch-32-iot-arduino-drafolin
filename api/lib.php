6dcf0b5fe54c768bf8f2f9cd4ba6a1f161c48a26
drafolin <derg@drafolin.ch>
2024-04-17 11:40:12 +0200

    added missed messages recovery

--- api/lib.php
+++ api/lib.php
@@ -1,6 +1,15 @@
 <?php
 require "./conf.php";
 
+function hexTo32Float($strHex) {
+    $strHex = implode('', array_reverse(str_split($strHex, 2)));
+    $hex = sscanf($strHex, "%02x%02x%02x%02x");
+    var_dump($hex);
+    $bin = implode('', array_map('chr', $hex));
+    $array = unpack("Gnum", $bin);
+    return $array['num'];
+}
+
 function getDeviceId(string $device_id): int|false
 {
 	global $conn;
@@ -31,9 +40,81 @@ function createDevice(string $device_id): int
 	return getDeviceId($device_id);
 }
 
+function verifyIntegrity(int $seq, string $device_id) {
+    global $conn;
+    $sql = "SELECT sequence_number, measure_time FROM measures m 
+        WHERE measure_time IN (
+            SELECT MAX(m.measure_time) as measure_time 
+            FROM measures m
+            WHERE m.device = ?
+            AND m.sequence_number IS NOT NULL
+        )
+        ";
+    $qry = $conn->prepare($sql);
+    if (!$qry->execute([$device_id])) {
+        echo $qry->error;
+        die (500);
+    }
+    $res = $qry->get_result()->fetch_all(MYSQLI_ASSOC);
+
+    if ($res[0]["sequence_number"] + 1 ==  $seq)
+        return;
+
+    $limit = $seq - ($res[0]["sequence_number"] + 1);
+    $since = date_create($res[0]["measure_time"]);
+    if(!$since)
+        die("invalid date");
+    $timestamp = (date_timestamp_get($since) + 1) * 1000;
+
+    var_dump($limit);
+
+    $ch = curl_init("https://api.sigfox.com/v2/devices/18E231/messages?since=$timestamp&limit=$limit");
+    $config = parse_ini_file("sigfox_config.ini");
+    curl_setopt($ch, CURLOPT_USERPWD, $config["USERNAME"] . ":" . $config["PASSWORD"]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $res=curl_exec($ch);
+    var_dump($res);
+    $res = json_decode($res, true)["data"];
+
+    usort($res, fn($a,$b) => $a["seqNumber"] - $b["seqNumber"]);
+
+    foreach ($res as $entry) {
+        $time = gmdate("Y-m-d H:i:s", $entry["time"]/1000);
+        $tempStr = substr($entry["data"], 0, 8);
+        $humStr = substr($entry["data"], 8, 8);
+        $temp = hexTo32Float($tempStr);
+        $hum = hexTo32Float($humStr);
+        $seq = $entry["seqNumber"];
+
+
+        $sql = "INSERT INTO measures VALUES (null, ?, ?, ?, ?, ?);";
+        $qry = $conn->prepare($sql);
+        if (!$qry->execute([$temp, $hum, $time, $seq, $device_id])) {
+            echo $qry->error;
+            die (500);
+        }
+
+    }
+
+    var_dump($res);
+}
+
 function saveMeasure(float $temp, float $hum, string $time, int $seq, int $device_id)
 {
 	global $conn;
+
+    $sql = "UPDATE measures 
+        SET sequence_number = NULL
+        WHERE sequence_number = ?
+        AND device = ?";
+    $qry = $conn->prepare($sql);
+    if (!$qry->execute([$seq, $device_id])) {
+        echo $qry->error;
+        die (500);
+    }
+
+    verifyIntegrity($seq, $device_id);
+
 	$sql = "INSERT INTO measures VALUES (null, ?, ?, ?, ?, ?);";
 	$qry = $conn->prepare($sql);
 	if (!$qry->execute([$temp, $hum, $time, $seq, $device_id])) {
--- api/migration.sql
+++ api/migration.sql
@@ -1,17 +1,24 @@
-DROP TABLE IF EXISTS `devices`; 
-CREATE TABLE `devices` (
-	`id` bigint(20) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	`device_id` VARCHAR(6) UNIQUE
-);
+USE `g61ai_32-iot-api`;
+
 
 drop table if exists `measures`;
 CREATE TABLE `measures` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `temperature` double(8,2) NOT NULL,
   `humidity` double(8,2) NOT NULL,
-  `measure_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `sequence_number` INT NOT NULL,
+  `measure_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sequence_number` INT,
   `device` BIGINT(20) unsigned,
-  FOREIGN KEY (`device`) REFERENCES `devices`(`id`),
   PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `devices`;
+CREATE TABLE `devices` (
+	`id` bigint(20) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`device_id` VARCHAR(6) UNIQUE
+);
+
+ALTER TABLE `measures`
+    ADD CONSTRAINT
+        FOREIGN KEY (`device`)
+        REFERENCES `devices`(`id`);
