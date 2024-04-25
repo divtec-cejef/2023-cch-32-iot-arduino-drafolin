27dc250b714a9a7ef95d798bac47d92530f2b4c0
Dråfølin <derg@drafolin.ch>
2024-04-25 15:51:01 +0200

    comments2

--- start/start.ino
+++ start/start.ino
@@ -9,26 +9,30 @@ const int SECOND = 1;
 const int MINUTE = 60 * SECOND;
 const int HOUR = 60 * MINUTE;
 
+// Configuration du senseur DHT
 DHT dht(5, DHT11);
-bool hasSent = false;
-int totalTime = 0;
 
+// Période configurée pour la boucle de l'envoi des données
 unsigned int loopDelaySeconds = HOUR;
 
 void setup() {
+  // Initialise et acquite la connexion Série
   Serial.begin(9600);
   Serial.println("Ready.");
 
+  // Initialise et acquite la connexion SigFox
   if (!SigFox.begin()) {
     Serial.println("Unable to init the Atmel 1DB4E6 Sigfox chipset");
     return;
   }
+  // Active l'utilisation de la LED orange de débug, qui clignote a l'envoi des données
   SigFox.debug();
 
+  // Initialise le senseur DHT
   dht.begin();
 
+  // Accroche le bouton d'envoi manuel
   pinMode(1, INPUT);
-
   LowPower.attachInterruptWakeup(1, SigFox_Data_Sender, PinStatus::RISING);
 }
 
@@ -39,8 +43,11 @@ void loop() {
 
 void GoToSleep() {
   Serial.println("MKR FOX 1200 - Going in sleep");
+  // Vide le tampon de la communication Série
   Serial.flush();
   Serial.end();
+
+  // Part en veille profonde
   LowPower.deepSleep((int)(loopDelaySeconds * 1000));
 }
 
@@ -55,11 +62,14 @@ void sendPacket(float temp, float hum) {
   SigFox.status();
   delay(1);
 
+  // Begins the SigFox packet
   SigFox.beginPacket();
   SigFox.write(temp);
   SigFox.write(hum);
+  // Ends the SigFox packet and waits for server response
   int status = SigFox.endPacket(true);
 
+  // Confirms the transmission of data
   if (status > 0) {
     Serial.println("No transmission");
   } else {
@@ -69,26 +79,42 @@ void sendPacket(float temp, float hum) {
   Serial.println(SigFox.status(SIGFOX));
   Serial.println(SigFox.status(ATMEL));
 
+  // Checks if we got a response from the server
   if (SigFox.parsePacket()) {
     std::stringstream message;
+
+    // Reads the whole SigFox downlink buffer
     while (SigFox.available()) {
-      message << std::setfill('0') << std::setw(2) << std::hex << SigFox.read();
+      // Remplis le nombre avec un 0 sur la droite pour correspondre à un octet
+      message << std::setfill('0') 
+              // Un octet = 2 caractères hexadécimaux
+              << std::setw(2) 
+              // transforme la lecture en hexadécimal
+              << std::hex 
+              // lis le prochain caractère
+              << SigFox.read();
     }
 
+    // remplace le paramètre de période par la valeur reçue
     message >> loopDelaySeconds;
 
   } else {
+    // Informe l'utilisateur que nous n'avons pas reçu de réponse du serveur
     Serial.println("Could not get any response from the server");
     Serial.println("Check the Sigfox coverage in your area");
     Serial.println(
         "If you are indoor, check the 20 dB coverage or move near a window");
   }
 
+  // Arrête le module SigFox
   SigFox.end();
 }
 
 void SigFox_Data_Sender() {
+  // Lance la communication série
   Serial.begin(9600);
+
+  // Initialise le module DHT
   dht.begin();
   delay(500);
   Serial.println("Begin data reading");
@@ -104,5 +130,6 @@ void SigFox_Data_Sender() {
   }
 
   Serial.println("Data read successfully");
+  // Sends the packet 
   sendPacket(temp, hum);
 }
